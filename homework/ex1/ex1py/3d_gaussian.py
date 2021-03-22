b3bc17a6d0629e5586b43ae30156d87bfc546222
mayb <maybibi@gmail.com>
2021-03-22 13:32:32 +0200

    ex1 files

---
+++ homework/ex1/ex1py/3d_gaussian.py
@@ -0,0 +1,51 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+from scipy.linalg import qr
+
+mean = [0, 0, 0]
+cov = np.eye(3)
+x_y_z = np.random.multivariate_normal(mean, cov, 50000).T
+
+def get_orthogonal_matrix(dim):
+    H = np.random.randn(dim, dim)
+    Q, R = qr(H)
+    return Q
+
+
+def plot_3d(x_y_z):
+    '''
+    plot points in 3D
+    :param x_y_z: the points. numpy array with shape: 3 X num_samples (first dimension for x, y, z
+    coordinate)
+    '''
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection='3d')
+    ax.scatter(x_y_z[0], x_y_z[1], x_y_z[2], s=1, marker='.', depthshade=False)
+    ax.set_xlim(-5, 5)
+    ax.set_ylim(-5, 5)
+    ax.set_zlim(-5, 5)
+    ax.set_xlabel('x')
+    ax.set_ylabel('y')
+    ax.set_zlabel('z')
+    return fig
+
+
+def plot_2d(x_y):
+    '''
+    plot points in 2D
+    :param x_y_z: the points. numpy array with shape: 2 X num_samples (first dimension for x, y
+    coordinate)
+    '''
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+    ax.scatter(x_y[0], x_y[1], s=1, marker='.')
+    ax.set_xlim(-5, 5)
+    ax.set_ylim(-5, 5)
+    ax.set_xlabel('x')
+    ax.set_ylabel('y')
+
+cov = np.array([[0.1,0,0],[0,0.5,0],[0,0,2]])*get_orthogonal_matrix(3)
+x_y_z = np.random.multivariate_normal(mean, cov, 50000).T
+fig = plot_3d(x_y_z)
+fig.savefig("G:\\My Drive\\Uni\\SecondYear\\SemesterB\\IML\\HUJI.IML\\homework\\ex1\\ex1py\\covOrt")
